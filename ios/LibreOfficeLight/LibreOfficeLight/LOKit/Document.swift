0c4c4f67b73e8b8b632678bd53e5914575a02109
Jon Nermut <jon.nermut@asdeqlabs.com>
2018-01-11 20:35:58 +1100

    iOS: Add LibreOffice licence header
    Change-Id: I281f3fd425a6f509b44a0a62ddbbda2d5c30092a

--- ios/LibreOfficeLight/LibreOfficeLight/LOKit/Document.swift
+++ ios/LibreOfficeLight/LibreOfficeLight/LOKit/Document.swift
@@ -1,9 +1,9 @@
 //
-//  Document.swift
-//  LibreOfficeKitIOS
+// This file is part of the LibreOffice project.
 //
-//  Created by Jon Nermut on 31/12/17.
-//  Copyright © 2017 LibreOffice. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
 
 import Foundation
--- ios/LibreOfficeLight/LibreOfficeLight/LOKit/Util.swift
+++ ios/LibreOfficeLight/LibreOfficeLight/LOKit/Util.swift
@@ -1,9 +1,9 @@
 //
-//  Util.swift
-//  LibreOfficeLight
+// This file is part of the LibreOffice project.
 //
-//  Created by Jon Nermut on 9/1/18.
-//  Copyright © 2018 jani. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
 
 import UIKit
